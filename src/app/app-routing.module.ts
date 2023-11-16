2c4e1c08b6a53bfda571a53f0edd9e5b3cebd8bf
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-11-16 13:52:10 +0300

    fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { MainComponent } from './pages/main/main.component';
 import { ContactsPageComponent } from './pages/contacts-page/contacts-page.component';
 import { ServicePageComponent } from './pages/service-page/service-page.component';
 import { ProductsPageComponent } from './pages/products-page/products-page.component';
+import { DrugsPageComponent } from './pages/drugs-page/drugs-page.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'contacts', component: ContactsPageComponent },
   { path: 'services', component: ServicePageComponent },
   { path: 'products', component: ProductsPageComponent },
+  { path: 'drugs', component: DrugsPageComponent },
 ];
 
 @NgModule({
---
+++ src/app/pages/drugs-page/drugs-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drugs-page',
+  templateUrl: './drugs-page.component.html',
+  styleUrls: ['./drugs-page.component.scss']
+})
+export class DrugsPageComponent {
+
+}
