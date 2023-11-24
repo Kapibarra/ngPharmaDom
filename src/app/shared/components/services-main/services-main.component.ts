318bef17bbe632df828405f3f704f731f923851b
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-11-24 13:37:40 +0300

    FIXES

--- src/app/shared/components/services-main/services-main.component.ts
+++ src/app/shared/components/services-main/services-main.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-services-main',
   templateUrl: './services-main.component.html',
-  styleUrls: ['./services-main.component.scss']
+  styleUrls: ['./services-main.component.scss'],
 })
 export class ServicesMainComponent {
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'ru', 'cn']);
+    translate.setDefaultLang('ru');
+    const browserLang = translate.getBrowserLang();
+    const langToUse =
+      browserLang && ['en', 'ru', 'cn'].includes(browserLang)
+        ? browserLang
+        : 'ru';
 
+    // Установите выбранный язык
+    translate.use(langToUse);
+  }
+  visible: boolean = false;
+
+  showDialog() {
+    this.visible = true;
+  }
 }
