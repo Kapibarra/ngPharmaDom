9bbf2f8d971fbef8b37a80818645b722077ce174
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-11-24 13:07:10 +0300

    fixes

--- src/app/pages/drugs-page/drugs-page.component.ts
+++ src/app/pages/drugs-page/drugs-page.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-drugs-page',
   templateUrl: './drugs-page.component.html',
-  styleUrls: ['./drugs-page.component.scss']
+  styleUrls: ['./drugs-page.component.scss'],
 })
 export class DrugsPageComponent {
+  downloadFile(fileName: string): void {
+    const fileUrl = `assets/downloads/${fileName}`;
 
+    fetch(fileUrl)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(blob);
+
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = fileName;
+        link.click();
+        link.addEventListener('click', () => {
+          window.URL.revokeObjectURL(url);
+        });
+      });
+  }
 }
