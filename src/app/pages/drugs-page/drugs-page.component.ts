9250a46d18030eb1a8aa8d05bd2356982201d89b
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-12-12 13:08:00 +0300

    update

--- src/app/pages/drugs-page/drugs-page.component.ts
+++ src/app/pages/drugs-page/drugs-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { SelectItem } from 'primeng/api';
 
 interface Medications {
@@ -88,11 +89,126 @@ export class DrugsPageComponent {
     Кальцимиметики: ['Цинакальцет'],
     Анальгетики: ['Парацетамол'],
   };
+  medicationsEn = {
+    Cardiology: [
+      'Atorvastatin',
+      'Rosuvastatin',
+      'Clopidogrel',
+      'Ibuprofen',
+      'Trimetazidine',
+      'Amlodipine',
+      'Torsemide',
+    ],
+    'Neurology and Psychiatry': [
+      'Sumatriptan succinate',
+      'Gabapentin',
+      'Amitriptyline',
+      'Betahistine',
+      'Tizanidine',
+      'Chlorpromazine',
+    ],
+    Oncology: [
+      'Everolimus',
+      'Darunavir',
+      'Meloxicam',
+      'Nilotinib',
+      'Erlotinib',
+    ],
+    Gastroenterology: [
+      'Metoclopramide',
+      'Amino salicylate sodium dihydrate',
+      'Domperidone',
+      'Mycophenolate sodium',
+      'Mycophenolate mofetil',
+      'Isoniazid',
+      'Domperidone',
+      'Meclofenamic acid',
+    ],
+    Immunosuppressant: ['Mycophenolate sodium', 'Darunavir', 'Oseltamivir'],
+    'Antiviral and Antiretroviral Agents': ['Darunavir', 'Oseltamivir'],
+    Asthma: ['Salbutamol', 'Fenoterol HBr'],
+    'Antitubercular Agents': ['Amino salicylate sodium dihydrate', 'Isoniazid'],
+    Anticoagulant: ['Clopidogrel'],
+    Antiemetic: ['Metoclopramide', 'Domperidone'],
+    'Tricyclic Antidepressant': ['Amitriptyline'],
+    Dermatology: ['Ornidazole', 'Posaconazole', 'Diclofenac'],
+    Antispasmodic: ['Drotaverine', 'Mebeverine'],
+    'Erectile Dysfunction Agent': ['Tamsulosin'],
+    'Antifungal Agent': ['Voriconazole', 'Posaconazole'],
+    Antibiotics: ['Moxifloxacin'],
+    'Anti-Inflammatory Agents': ['Aceclofenac', 'Diclofenac'],
+    Antidiabetic: ['Metformin'],
+    'Anticholinergic Agent': ['Fenpiverinium bromide'],
+    'Allergy and Immunology': ['Pitofenone'],
+    Osteoporosis: ['Alendronate sodium'],
+    Calcimimetics: ['Cinacalcet'],
+    Analgesics: ['Paracetamol'],
+  };
+  medicationsCn = {
+    心脏病学: [
+      '阿托伐他汀',
+      '瑞舒伐他汀',
+      '氯吡格雷',
+      '布洛芬',
+      '曲美他嗪',
+      '氨氯地平',
+      '托塞米特',
+    ],
+    神经病学和精神病学: [
+      '舍曲坦琼酸盐',
+      '加巴喷丁',
+      '阿米替林',
+      '贝他组胺',
+      '替扎尼丁',
+      '氯丙嗪',
+    ],
+    肿瘤学: ['依维莫司', '达瑞那韦', '美洛昔康', '尼洛替尼', '厄洛替尼'],
+    胃肠病学: [
+      '多巴胺',
+      '氨基水杨酸钠二水合物',
+      '多潘立酮',
+      '麦考酚酸钠',
+      '麦考酚酸酯',
+      '异烟肼',
+      '多潘立酮',
+      '美克洛酚酸',
+    ],
+    免疫抑制剂: ['麦考酚酸钠', '达瑞那韦', '奥司他韦'],
+    抗病毒和抗逆转录病毒药物: ['达瑞那韦', '奥司他韦'],
+    哮喘: ['沙丁胺醇', '非诺特罗 HBr'],
+    抗结核药物: ['氨基水杨酸钠二水合物', '异烟肼'],
+    抗凝血剂: ['氯吡格雷'],
+    止吐药: ['多潘立酮', '麦考酚酸钠'],
+    三环抗抑郁药: ['阿米替林'],
+    皮肤科: ['奥尼达唑', '泊沙康唑', '双氯芬酸'],
+    解痉药: ['丙哌酮', '美贝韦林'],
+    勃起功能障碍药物: ['坦索洛辛'],
+    抗真菌药物: ['伏立康唑', '泊沙康唑'],
+    抗生素: ['莫西沙星'],
+    抗炎药: ['乙酰氨基酚', '双氯芬酸'],
+    抗糖尿病药物: ['二甲双胍'],
+    抗胆碱药物: ['溴隐亭溴化物'],
+    过敏和免疫学: ['匹托酚酮'],
+    骨质疏松症: ['阿伦膦酸钠'],
+    钙调素: ['西那卡塞'],
+    镇痛药: ['对乙酰氨基酚'],
+  };
 
   selectedCategory: string = 'Кардиология';
-
   selectedMedication: string = '';
 
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'ru', 'cn']);
+    translate.setDefaultLang('ru');
+    const browserLang = translate.getBrowserLang();
+    const langToUse =
+      browserLang && ['en', 'ru', 'cn'].includes(browserLang)
+        ? browserLang
+        : 'ru';
+
+    // Установите выбранный язык
+    translate.use(langToUse);
+  }
   downloadFile(fileName: string): void {
     const fileUrl = `assets/downloads/${fileName}`;
 
